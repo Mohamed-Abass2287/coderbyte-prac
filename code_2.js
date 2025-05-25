64ca3b0f71e0bb705354412edce287b2a6dec365
mohamed abass tawana <aminabass987@gmail.com>
2025-05-25 03:08:54 +0300

    js

---
+++ code_2.js
@@ -0,0 +1,14 @@
+function FindIntersection(strArr) {
+    // Convert input strings into arrays of numbers
+    const list1 = new Set(strArr[0].split(", ").map(Number));
+    const list2 = new Set(strArr[1].split(", ").map(Number));
+
+    // Find the intersection and sort it
+    const intersection = [...list1].filter(num => list2.has(num)).sort((a, b) => a - b);
+
+    return intersection.length > 0 ? intersection.join(",") : "false";
+}
+
+// Example usage:
+console.log(FindIntersection(["1, 3, 4, 7, 13", "1, 2, 4, 13, 15"]));
+// Expected Output: "1,4,13"
